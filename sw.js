4d20072568ad91c57ca330813cef04fc5d03722e
sawadayuki <sawadayuki88@gmail.com>
2021-03-29 06:59:29 +0900

    add Workbox and cache in worker

--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
 self.addEventListener('fetch',function(e) {
     //空でもokらしい
 });
+
+// ファイブラリのインポート
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+
+// ファイルのキャッシュ
+workbox.precaching.precacheAndRoute([
+  {
+    url: '/index.html',
+    revision: '1'
+  },
+  {
+    url: '/style.css',
+    revision: '1'
+  },
+  { 
+　url: '/color.js',
+    revision: '1'
+  },
+])
