64089c05c8e6f5c2acd157ad61a0af2258052dd6
sawadayuki <sawadayuki88@gmail.com>
2021-03-29 15:01:07 +0900

    notification info added

--- notification.js
+++ notification.js
@@ -1,18 +1,21 @@
 Notification.requestPermission().then((result) =>{
     if (result == 'granted') {
-        testNotification();
+        testNotification('first');
     } else if (result == 'denied') {
         console.log('notificatin denied');
     } else if (result == 'default'){
         console.log('notificatin ignored');
+    } else {
+        testNotification('else');
     }
+    console.log('request permission');
 });
 
 function testNotification(params) {
     const title = "New Notification!!";
 
     const option = {
-        body: 'Test notification desu!!',
+        body: 'Test '+params+' notification desu!!',
         icon: 'icons/icon-192x192.png'
     }
 
